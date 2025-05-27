b489f0ca2c70a9c50b409308f58122e15c3c1386
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2025-05-27 14:06:01 -0300

    orquestrador

--- Src/main.py
+++ Src/main.py
@@ -1,7 +1,9 @@
 from Tools.Elastic.elastic import Elasticsearch_up
 from Tools.Splunk.splunk import Splunk_up
+from Tools.TheHive.thehive import Thehive
 
 if __name__ == "__main__":
     splunk = Splunk_up()
     elasticsearch_up = Elasticsearch_up()
-    elasticsearch_up.upload(splunk.searcher())
+    thehive_client = Thehive()
+    thehive_client.create_alert_function(elasticsearch_up.searcher)    
