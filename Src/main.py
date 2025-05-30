892408ba015c7555493e7dde8f2c5b4541bff3e1
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2025-05-30 13:31:04 -0300

    Add Server connection
    Server connection. It execute a suricata comand

--- Src/main.py
+++ Src/main.py
@@ -1,8 +1,12 @@
 from Tools.Elastic.elastic import Elasticsearch_up
 from Tools.Splunk.splunk import Splunk_up
 from Tools.TheHive.thehive import Thehive
+from Tools.Server.server_connector import Server
+
 
 if __name__ == "__main__":
+    server = Server()
+    server.suricata_datas
     splunk = Splunk_up()
     elasticsearch_up = Elasticsearch_up()
     thehive_client = Thehive()
