3218e765a9e6784c81770dd4c06c75d98a20e313
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2025-06-04 10:12:11 -0300

    Improve the code
    Improve the code readability

--- Src/Tools/Server/server_connector.py
+++ Src/Tools/Server/server_connector.py
@@ -1,18 +1,17 @@
 import paramiko
 import os
 from dotenv import load_dotenv
-import pandas as pd
 
 class Server:
     def __init__(self):
         load_dotenv()
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.client.connect("IP Address", port=22, username="user", password="password")
+        self.client.connect(os.getenv("server_ip"), port=22, username=os.getenv("server_user"), password=os.getenv("server_password"))
     
     @property
     def suricata_datas(self):
         
         self.client.exec_command(f"suricata -r pcap_datas/2024-09-19-file-downloader-to-Lumma-Stealer.pcap")
         sftp = self.client.open_sftp()
-        sftp.get(f"{path}eve.json",f"{os.getcwd()}/eve.json")
+        sftp.get(f"{os.getenv("remote_path")}/eve.json",f"{os.getcwd()}/eve.json")
