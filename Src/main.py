6c587b5d5fbfbc2a452d8ae5e86e900e19fb1430
kaykRodr1gu3s <110197812+kaykRodr1gu3s@users.noreply.github.com>
2025-06-04 09:55:24 -0300

    orchestrator
    This code is the orchestrator, it adds all the functions in and interconnects all the classes and functions

--- Src/main.py
+++ Src/main.py
@@ -1,3 +1,4 @@
+import json
 from Tools.Elastic.elastic import Elasticsearch_up
 from Tools.Splunk.splunk import Splunk_up
 from Tools.TheHive.thehive import Thehive
@@ -5,9 +6,23 @@ from Tools.Server.server_connector import Server
 
 
 if __name__ == "__main__":
+
     server = Server()
     server.suricata_datas
     splunk = Splunk_up()
     elasticsearch_up = Elasticsearch_up()
+    splunk.suricata_alerts_upload()
+
+    for docs in splunk.searcher:
+        
+        doc = docs.get("_raw")
+        doc = json.loads(doc)
+        verification = elasticsearch_up.id_identifier(doc.get("flow_id"))
+
+        if verification == False:
+            elasticsearch_up.upload(doc)
+
+
+    elasticsearch_up.searcher
     thehive_client = Thehive()
-    thehive_client.create_alert_function(elasticsearch_up.searcher)    
+    thehive_client.create_alert_function(elasticsearch_up.searcher)
